5dba279ebef11b3eb823217f2fe031b264878747
kotamadeo <kotamadeo@gmail.com>
2022-07-25 14:48:25 +0300

    Fixes by mentor's commentaries. Version 2.0.

--- src/main/java/com/gmail/at/kotamadeo/configuration/FaviconConfig.java
+++ src/main/java/com/gmail/at/kotamadeo/configuration/FaviconConfig.java
@@ -16,14 +16,14 @@ public class FaviconConfig {
     public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
         SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
         mapping.setOrder(Integer.MIN_VALUE);
-        mapping.setUrlMap(Collections.singletonMap("icons8-pixel-cat-100.png", faviconRequestHandler()));
+        mapping.setUrlMap(Collections.singletonMap("/favicon.ico", faviconRequestHandler()));
         return mapping;
     }
 
     @Bean
     protected ResourceHttpRequestHandler faviconRequestHandler() {
         ResourceHttpRequestHandler requestHandler = new ResourceHttpRequestHandler();
-        ClassPathResource classPathResource = new ClassPathResource("WEB-INF/views/img/");
+        ClassPathResource classPathResource = new ClassPathResource("src/main/WEB-INF/views/img/");
         List<Resource> locations = List.of(classPathResource);
         requestHandler.setLocations(locations);
         return requestHandler;
